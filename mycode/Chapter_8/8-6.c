f7c583eacfccd95c3163b8c8c829fcada03772c8
Quenkar <15172118655@163.com>
2020-07-02 13:00:30 +0800

    wait()&waitpid()

---
+++ mycode/Chapter_8/8-6.c
@@ -0,0 +1,32 @@
+#include <apue.h>
+#include <sys/wait.h>
+
+int main(void)
+{
+    pid_t pid;
+    int status;
+
+    if ((pid = fork()) < 0)
+    {
+        err_sys("fork error");
+    }
+    else if (pid == 0)
+    {
+        if ((pid == fork()) < 0)
+            err_sys("fork error");
+        else if (pid > 0)
+        {
+            exit(2);
+        }
+
+        sleep(2);
+        printf("parent pid = %ld\n", (long)getppid());
+        exit(3);
+    }
+
+    if (waitpid(pid, &status, 0) != pid)
+        err_sys("waitpid error");
+    pr_exit(status);
+
+    exit(0);
+}
